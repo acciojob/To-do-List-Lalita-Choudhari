8c0cb21f730a9b18a0192227c43564e2ba7d725a
Lalita-Choudhari <147739736+Lalita-Choudhari@users.noreply.github.com>
2024-02-06 20:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ol = document.querySelector("ol");
+
+btn.addEventListener("click",function() {
+   let item = document.createElement("li");
+	item.innerText= inp.value;
+	ol.appendChild(item);
+	inp.value="";
+})
 
